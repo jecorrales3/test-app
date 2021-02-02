26c5d3cfca83979495a78ed215bd3db27df27e12
Johan Corrales <jecorrales3@gmail.com>
2021-02-01 20:14:43 -0500

    merge

---
+++ Backend/api/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+//Settings
+const { config } = require('./config/index');
+
+//Required routes
+const authApi = require('./routes/auth');
+
+//Middleware
+const {
+  logErrors,
+  wrapErrors,
+  errorHandler,
+} = require('./utils/middleware/errorHandlers');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
+
+//Body parser
+app.use(express.json());
+
+//Routes
+authApi(app);
+
+//Catch 404
+app.use(notFoundHandler);
+
+//Errors middleware
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
+
+//Server listening
+app.listen(config.port, function () {
+  console.log(`Listening on http://localhost:${config.port}`);
+});
---
+++ Backend/api/utils/schemas/users.js
@@ -0,0 +1,19 @@
+const joi = require('@hapi/joi');
+
+const userIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/);
+const userNameSchema = joi.string().max(100);
+const userEmailSchema = joi.string().email();
+const userPasswordSchema = joi.string().max(100);
+const userRoleSchema = joi.string();
+
+const createUserSchema = {
+  name: userNameSchema.required(),
+  email: userEmailSchema.required(),
+  password: userPasswordSchema.required(),
+  userRole: userRoleSchema.required(),
+};
+
+module.exports = {
+  userIdSchema,
+  createUserSchema,
+};
